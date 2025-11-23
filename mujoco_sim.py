3a386336563b391a391421c4066ea37b917e99b8
Jing Cao <jcao0715@gmail.com>
2025-11-23 17:12:53 -0500

    rest and home position for cube and robot

--- mujoco_sim.py
+++ mujoco_sim.py
@@ -1,10 +1,21 @@
 import mujoco
-from mujoco import MjModel, MjData
+from mujoco import MjModel, MjData, mj_resetDataKeyframe
 import mujoco.viewer as viewer
 
 model = MjModel.from_xml_path("trs_so_arm100/scene.xml")
 data = MjData(model)
 
-with viewer.launch_passive(model, data):
-    while True:
+
+def reset_to_keyframe(name: str) -> bool:
+    key_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_KEY, name)
+    if key_id >= 0:
+        mj_resetDataKeyframe(model, data, key_id)
+        return True
+    return False
+
+reset_to_keyframe("rest_with_cube")
+
+with viewer.launch_passive(model, data) as gui:
+    while gui.is_running():
         mujoco.mj_step(model, data)
+        gui.sync()
